d68fc733e4bd496d047b6a9d1f9e3ef4c7c55e31
whatisloveT <whatislove56@icloud.com>
2022-08-06 21:56:38 +0500

    create serializer

---
+++ todonotes/Users/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+from .models import CustomUser
+
+
+class CustomUserModelSerializer(ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'first_name', 'last_name', 'email']
--- todonotes/todonotes/urls.py
+++ todonotes/todonotes/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from Users.views import CustomUserModelViewSet
+
+
+router = DefaultRouter()
+router.register('Users',CustomUserModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/',include('rest_framework.urls')),
+    path('api/', include(router.urls))
 ]
