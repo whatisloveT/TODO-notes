593d86cd4ac194e1afc2a337c6a7ad9116f0f109
whatisloveT <whatislove56@icloud.com>
2022-08-11 19:46:18 +0500

    create page to display a list of users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios'
+import UserList from './components/Users.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            'users' : []
+        }
+    }
+
+
+    componentDidMount() {
+        axios
+            .get('http://127.0.0.1:8000/api/Users/')
+            .then(response =>{
+                const users = response.data
+                this.setState(
+                    {
+                        'users' : users
+                    }
+                )
+
+            })
+            .catch(error => console.log(error))
+    }
+    render(){
+        return(
+            <div>
+                <UserList users = {this.state.users} />
+            </div>
+        )
+    }
 }
 
 export default App;
--- todonotes/todonotes/settings.py
+++ todonotes/todonotes/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Users',
+    'corsheaders',
     'rest_framework'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -51,6 +53,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:3000',
+]
 ROOT_URLCONF = 'todonotes.urls'
 
 TEMPLATES = [
